81751aa107afd86a39c5b860397e0380183c4773
David A.D. Morano <david.a.morano@gmail.com>
2018-12-11 14:39:29 -0500

    typo

--- libdam/willAddOver.hh
+++ libdam/willAddOver.hh
@@ -3,12 +3,30 @@
 
 /* revision history:
 
-	= 2012-11-21, David A­D­ Morano
+	= 2012-11-21, David AÂ­DÂ­ Morano
 	I took this from some of my previous code.
 
 */
 
-/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+/* Copyright Â© 2000 David AÂ­DÂ­ Morano.  All rights reserved. */
+
+/******************************************************************************
+
+	This generic subroutine will determine is an addition will overflow.
+
+	Synopsis:
+	int willAddOver(T n1, T n2)
+
+	Arguments:
+	n1		first number to add
+	n2		second number to add
+
+	Returns:
+	0 (false)	will not overflow
+	1 (true)	will overflow
+
+
+******************************************************************************/
 
 
 #include	<envstandards.h>
@@ -21,9 +39,10 @@ template<T>
 int willAddOver(T n1,T n2)
 {
 	const int	s = sizeof(T) ;
-	T		min, max ;
+	T		min = 0 ;
+	T		max = 0 ;
 	int		f = FALSE ;
-	witch (s) {
+	switch (s) {
 	case 1:
 	    min = SCHAR_MIN ;
 	    max = SCHAR_MAX ;
@@ -40,9 +59,13 @@ int willAddOver(T n1,T n2)
 	    min = LONG_MIN ;
 	    max = LONG_MAX ;
 	    break ;
+	case 16:
+	    break ; /* guess that it will be OK */
 	} /* end switch */
-	f = f || (n1 > 0) && (n2 > 0) && (n1 > (max - n2)) ;
-	f = f || (n1 < 0) && (n2 < 0) && (n1 < (min - n2)) ;
+	if (max != 0) {
+	    f = f || (n1 > 0) && (n2 > 0) && (n1 > (max - n2)) ;
+	    f = f || (n1 < 0) && (n2 < 0) && (n1 < (min - n2)) ;
+	}
 	return f ;
 }
 /* end subroutine (willAddOver) */
