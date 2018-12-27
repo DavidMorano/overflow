0eb12cb666ec6188600164f1b0da23a604e6affd
David A.D. Morano <david.a.morano@gmail.com>
2018-12-27 05:48:55 -0500

    typo in comment

--- libdam/willAddOver.hh
+++ libdam/willAddOver.hh
@@ -14,7 +14,7 @@
 
 /******************************************************************************
 
-	This generic subroutine will determine is an addition will overflow.
+	This generic subroutine will determine if an addition will overflow.
 
 	Synopsis:
 	int willAddOver(T n1, T n2)
