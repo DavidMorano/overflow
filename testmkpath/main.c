b856e28eaf14a3b131183204dc9b0cc08b2e1847
David A.D. Morano <david.a.morano@gmail.com>
2019-01-07 13:02:54 -0500

    smnall clean-up

--- testmkpath/main.c
+++ testmkpath/main.c
@@ -1,11 +1,14 @@
 /* main */
 
+/* test for |mkpath(3dam)| */
 
+#include	<envstandards.h>
 
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<limits.h>
 #include	<stdio.h>
+#include	<localmisc.h>		/* extra types */
 
 
 /* external subroutines */
@@ -17,53 +20,34 @@ extern int	mkpath4(char *,const char *,const char *,const char *,
 			const char *) ;
 
 
+/* exported subroutines */
 
 
-
-int main(argc,argv,envv)
-int	argc ;
-char	*argv[] ;
-char	*envv[] ;
+/* ARGSUSED */
+int main(int argc,cchar **argv,cchar **envv)
 {
-	char	buf[MAXPATHLEN + 1] ;
+	char	buf[MAXPATHLEN + 1] = { 0 } ;
 
-
-	buf[0] = '\0' ;
 	if (argc > 1) {
-
 	    switch (argc) {
-
 	    case 2:
 	        mkpath1(buf,argv[1]) ;
-
 	        break ;
-
 	    case 3:
 	        mkpath2(buf,argv[1],argv[2]) ;
-
 	        break ;
-
 	    case 4:
 	        mkpath3(buf,argv[1],argv[2],argv[3]) ;
-
 	        break ;
-
 	    case 5:
 	        mkpath4(buf,argv[1],argv[2],argv[3],argv[4]) ;
-
 	        break ;
-
 	    } /* end switch */
-
 	    fprintf(stdout,"%s\n",buf) ;
-
 	} /* end if */
-
-	fclose(stdout) ;
 
 	return 0 ;
 }
 /* end subroutine (main) */
-
 
 
