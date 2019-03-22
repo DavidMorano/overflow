b12915ce2068ada2d26d9741cd1e8b92ef6f6292
David A.D. Morano <david.a.morano@gmail.com>
2019-03-22 17:58:34 -0400

    tiny pretty

--- testmisc/mainstrchr.c
+++ testmisc/mainstrchr.c
@@ -1,28 +1,41 @@
 /* main (strchr) */
 
+/* test to see how |strchr(3c)| reacts to 8-bit character */
+
 
 /* revision history:
 
-	= 2017-09-07, David A­D­ Morano
+	= 2017-09-07, David AÂ­DÂ­ Morano
 	Originally written for Rightcore Network Services.
 
 */
 
-/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright Â© 2017 David AÂ­DÂ­ Morano.  All rights reserved. */
 
+/*****************************************************************************
+
+	This test is *not* conclusive, because we do not know the signedness
+	of the type 'char' in all compilation environments.
+
+	We are also not testing our own |nchr(3dam)| function ... because we
+	already know that it works correctly! Haha.
+
+
+*****************************************************************************/
 
 #include	<envstandards.h>
 #include	<string.h>
 #include	<stdio.h>
 #include	<localmisc.h>
+
 extern int	nchr(const char *,int,int) ;
 
 int main()
 {
-	const char	*a = "this­that­other" ;
+	const int	sch = MKCHAR('Â­') ;
+	int		n = 0 ;
+	const char	*a = "thisÂ­thatÂ­other" ;
 	const char	*sp, *tp ;
-	int	n = 0 ;
-	int	sch = MKCHAR('­') ;
 
 	sp = a ;
 	while ((tp = strchr(sp,sch)) != NULL) {
